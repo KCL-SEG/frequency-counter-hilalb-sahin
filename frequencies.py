f977b8a0cac6913847186b66cf8cdf6cde797db7
Hilal <hilal.sahin@kcl.ac.uk>
2023-10-01 09:45:16 +0100

    completed the code

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,20 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+
+    #  inside the frequencies add the item , if it already exists, then increase it
+    for item in items:
+        #converting all to string 
+        item_str = str(item)
+
+        if item_str in frequencies:
+            #increase the existing value by 1 
+            frequencies[item_str]+=1
+        else:
+            frequencies[item_str]= 1
+            
+
     return frequencies
+
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
